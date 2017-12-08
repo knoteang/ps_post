7e532017eef4335a04cb4d74c628d6a977ac06fc
Swiss <swiss.knot@swu.ac.th>
2017-12-08 20:23:34 +0700

    fix callback

--- config/env/development.js
+++ config/env/development.js
@@ -7,16 +7,16 @@ module.exports = {
   google: {
     clientID: '498077320069-dkhdsb23vhmfq1edunbdf2vjc5gt79ks.apps.googleusercontent.com',
     clientSecret: 'f6V9wdARvM-GdX6FNzRLhMpG',
-    callbackURL: 'http://localhost:3000/oauth/google/callback'
+    callbackURL: 'https://pspost.herokuapp.com/oauth/google/callback'
   },
   facebook: {
     clientID: '1740364465996430',
     clientSecret: 'f72838ff6b2abd2368b1293331f0c002',
-    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    callbackURL: 'https://pspost.herokuapp.com/oauth/facebook/callback'
   },
   instagram: {
     clientID: '7a7b866454cd4bc08972a2f66358452a',
     clientSecret: '1df597291b11468cb6133db7152a29f1',
-    callbackURL: 'http://localhost:3000/oauth/instagram/callback'
+    callbackURL: 'https://pspost.herokuapp.com/oauth/instagram/callback'
   }
 }
