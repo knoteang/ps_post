d1642080b6d9f8c0998ca46e504f0d5a88e38a08
Swiss <swiss.knot@swu.ac.th>
2017-12-09 15:25:39 +0700

    fix schema post

--- app/models/post.model.js
+++ app/models/post.model.js
@@ -2,23 +2,23 @@ import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-       title: {
-              type: String,
-              required: true,
-              trim: true
-       },
-       content: {
-              type: String
-       },
-             author: {
-              type: String,
-              required: true,
-              trim: true
-       },
-       time: {
-              type: Date,
-              default: Date.now
-       }
+      title: {
+            type: String,
+            required: true,
+            trim: true
+      },
+      content: {
+            type: String
+      },
+      author: {
+            type: String,
+            required: true,
+            trim: true
+      },
+      time: {
+            type: Date,
+            default: Date.now
+      }
 });
 
 mongoose.model('Post', PostSchema);
