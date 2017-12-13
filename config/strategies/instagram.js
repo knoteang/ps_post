d7974c84291ef875b1fbb535047c024b77a36c3a
Swiss <swiss.knot@swu.ac.th>
2017-12-13 20:42:57 +0700

    fix fuc oauth

--- config/strategies/instagram.js
+++ config/strategies/instagram.js
@@ -23,6 +23,7 @@ module.exports = () => {
       providerId: profile.id,
       providerData: providerData
     }
+    console.log("123456789")
     user.saveOAuthUserProfile(req, providerUserProfile, done);
   }));
 }
