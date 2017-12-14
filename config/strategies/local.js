ebdf663b5bb1c00b2b3cf2c94c05ac804136fc6b
Swiss <swiss.knot@swu.ac.th>
2017-12-14 13:19:46 +0700

    fix local

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -4,7 +4,7 @@ var LocalStratege = localStratege.Strategy;
 var User = require('mongoose').model('User');
 
 module.exports = () => {
-  passport.use(new LocalStratege((username, password, done) => {
+  passport.use(req, res, new LocalStratege((username, password, done) => {
     User.findOne({ username: username }, (err, user) => {
       if (err) { return done(err); }
       if (!user || user.provider != 'local' || !user.authenticate(password)) {
