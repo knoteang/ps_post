75618c5252a18ebf6481ae2c02ec67e72e85f1a5
Swiss <swiss.knot@swu.ac.th>
2017-12-14 19:02:08 +0700

    fix crreate post

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -2,63 +2,47 @@ var Post = require('mongoose').model('Post');
 var path = require("path");
 
 exports.getAll = (req, res, next) => {
-      if (req.user) {
-            Post.find((err, data) => {
-                  if (err) {
-                        console.log('Failure: ' + err);
-                        return next(err);
-                  }
-                  else {
-                        console.log(data);
-                        res.json(data);
-                  }
-            });
-      }
-      else {
-            res.status(400).send({
-                  message: 'User is not signed in'
-            });
-      }
+
+      Post.find((err, data) => {
+            if (err) {
+                  console.log('Failure: ' + err);
+                  return next(err);
+            }
+            else {
+                  console.log(data);
+                  res.json(data);
+            }
+      });
+
 }
 exports.getOne = (req, res, next) => {
-      if (req.user) {
-            Post.findOne({ _id: req.params.id }, (err, data) => {
-                  if (err) {
-                        console.log('Failure: ' + err);
-                        return next(err);
-                  }
-                  else {
-                        console.log(data);
-                        res.json(data);
-                  }
-            });
-      }
-      else {
-            res.status(400).send({
-                  message: 'User is not signed in'
-            });
-      }
+
+      Post.findOne({ _id: req.params.id }, (err, data) => {
+            if (err) {
+                  console.log('Failure: ' + err);
+                  return next(err);
+            }
+            else {
+                  console.log(data);
+                  res.json(data);
+            }
+      });
+
 }
 exports.getMyPost = (req, res, next) => {
-      if (req.user) {
-            var username = req.user.username;
-            console.log(username);
-            Post.find({ author: username }, (err, data) => {
-                  if (err) {
-                        console.log('Failure: ' + err);
-                        return next(err);
-                  }
-                  else {
-                        console.log(data);
-                        res.json(data);
-                  }
-            }).sort({ time: 'desc' });
-      }
-      else {
-            res.status(400).send({
-                  message: 'User is not signed in'
-            });
-      }
+
+      var username = req.user.username;
+      console.log(username);
+      Post.find({ author: username }, (err, data) => {
+            if (err) {
+                  console.log('Failure: ' + err);
+                  return next(err);
+            }
+            else {
+                  console.log(data);
+                  res.json(data);
+            }
+      }).sort({ time: 'desc' });
 }
 exports.create = (req, res, next) => {
 
