d1e74acc3621fd79c44f3df9666c8fcb0f2290ec
Swiss <swiss.knot@swu.ac.th>
2017-12-16 01:43:54 +0700

    fix

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -16,7 +16,7 @@ exports.getUsers = (req, res, next) => {
 
 exports.getUserOne = (req, res, next) => {
     User.findOne({
-        username: req.params.username
+        _id: req.params.username
     }, function (err, user) {
         if (user) {
             console.log('Success');
